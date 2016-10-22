90ebbab75f12fc5bc824f371e1a20bb3b4cadc00
Dzsldwn <741409033@qq.com>
2016-10-22 21:17:15 +0800

    上次没有提交成功，这次重新提交！

--- app/static/js/calendar/createMovieEvent.js
+++ app/static/js/calendar/createMovieEvent.js
@@ -6,29 +6,32 @@ $(document).ready(function () {
         var value = $(this).val();
         //判断条件之后可修改
         if (value != "") {
+            $("#select").find("p.content").text(function () {
+                return $(this).parents().attr("data-id");
+            });
             $("#select").slideDown();
         }else {
             $("#select").slideUp();
         }
     });
 
-    $("#select .weui_cell").hover(function () {
-        $(this).css({"background-color":"#1ebc21","color":"white"});
-        // console.log("ok");
+    $("#select").children("div").hover(function () {
+        $(this).addClass("touch");
     }, function () {
-        $(this).css({"background-color":"white","color":"black"});
+        $(this).removeClass("touch");
     });
 
-    $("#select .weui_cell").click(function () {
-        var str = $(this).attr('data-id');
+    $("#select").children("div").click(function () {
+        var str = $(this).attr("data-id");
         $(".input").val(str);
+        $("#select").slideUp(1);
     });
-    
-    // $("#select div").on("touchstart", function (e) {
-    //     $(this).css({"background-color":"#1ebc21","color":"white"});
-    // });
-    // $("#select div").on("touchend", function (e) {
-    //     $(this).css({"background-color":"white","color":"black"});
-    // });
 
+    $("#select").children("div").on("touchstart", function () {
+        $(this).addClass("touch");
+    });
+
+    $("#select").children("div").on("touchend", function () {
+        $(this).removeClass("touch");
+    });
 });
