2d004c79b0c75b783a9f91978f9a4b0f2a0496e7
JunqiLin <434684256@qq.com>
2016-12-29 16:11:03 +0800

    1.电影详情页面修改

--- app/static/js/movie/detailInfo.js
+++ app/static/js/movie/detailInfo.js
@@ -22,12 +22,26 @@ $(document).ready(function () {
 
 //      $('.record_title').click(function () {
     $('.record_click').click(function () {
+
         $(this).next().slideToggle();
+
+         if($(this).children().eq(1).hasClass('disvi')){
+            $(this).children().eq(1).removeClass('disvi');
+            console.log("remove class");
+            }
+         else{
+            $(this).children().eq(1).addClass('disvi');
+            console.log("add class");
+            }
+
+//       $(this).children().eq(1).addClass("disvi");
         if (0 == click) {
             $(this).css({'background':'rgba(255,255,255, 0.8)'});
+
             click = 1;
         } else {
             $(this).css({'background':'#fff'});
+
             click = 0;
         }
     });
